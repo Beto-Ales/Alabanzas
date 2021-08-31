9a4a00b01b63a79d5df092dfbfe1449ef427b6e6
Alberto Alesandroni <betoalesandroni@gmail.com>
2021-08-31 16:43:57 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,551 @@
+// all the songs
+// -------------
+const songs = [];
+
+// set progress to 0
+let progress = 0;
+
+// set songs index
+let song = 0;
+
+
+
+
+
+
+
+
+// load lyrics for song 1
+// ----------------------
+
+const todasLasPromesas = [];
+
+todasLasPromesas.push(`<h1>
+Todas las promesas del Señor Jesús<br/>
+son apoyo poderoso de mi fe<br/>
+mientras viva aquí cercado de su luz<br/>
+siempre en sus promesas confiaré
+</h1>`);
+
+todasLasPromesas.push(`<h1>
+Grandes fieles<br/>
+las promesas que el Señor Jesús ha dado<br/>
+Grandes fieles<br/>
+en ellas para siempre confiaré
+</h1>`);
+
+todasLasPromesas.push(`<h1>
+Todas sus promesas para el hombre fiel<br/>
+el Señor en sus bondades cumplirá<br/>
+y confiado sé que para siempre en Él<br/>
+paz eterna mi alma gozará
+</h1>`);
+
+todasLasPromesas.push(`<h1>
+Grandes fieles<br/>
+las promesas que el Señor Jesús ha dado<br/>
+Grandes fieles<br/>
+en ellas para siempre confiaré
+</h1>`);
+
+todasLasPromesas.push(`<h1>
+Todas las promesas del Señor serán<br/>
+gozo y fuerza en nuestra vida terrenal<br/>
+ellas en la dura lid nos sostendrán<br/>
+y triunfar podremos sobre el mal
+</h1>`);
+
+todasLasPromesas.push(`<h1>
+Grandes fieles<br/>
+las promesas que el Señor Jesús ha dado<br/>
+Grandes fieles<br/>
+en ellas para siempre confiaré
+</h1>`);
+
+
+// include new song
+songs.push(todasLasPromesas);
+
+
+
+
+
+// load lyrics for song 2
+// ----------------------
+const granGozoHayEnMiAlmaHoy = [];
+
+granGozoHayEnMiAlmaHoy.push(`<h1>
+Gran gozo hay en mi alma hoy:<br/>
+Jesús conmigo está;<br/>
+contento con su amor estoy,<br/>
+su dulce paz me da.<br/>
+</h1>`);
+
+granGozoHayEnMiAlmaHoy.push(`<h1>
+Coro<br/>
+Brilla el sol de Cristo en mi alma;<br/>
+cada día voy feliz así.<br/>
+Su faz sonriente al contemplar,<br/>
+¡cuánto gozo siento en mí!<br/>
+</h1>`);
+
+granGozoHayEnMiAlmaHoy.push(`<h1>
+En mi alma hay melodías hoy,<br/>
+canciones a mi Rey.<br/>
+Feliz y libre en Cristo soy<br/>
+y salvo por la fe.<br/>
+</h1>`);
+
+granGozoHayEnMiAlmaHoy.push(`<h1>
+Coro<br/>
+Brilla el sol de Cristo en mi alma;<br/>
+cada día voy feliz así.<br/>
+Su faz sonriente al contemplar,<br/>
+¡cuánto gozo siento en mí!<br/>
+</h1>`);
+
+granGozoHayEnMiAlmaHoy.push(`<h1>
+Paz plena tengo en mi alma hoy,<br/>
+pues Cristo me salvó.<br/>
+Mis hierros rotos quedan ya:<br/>
+Jesús me libertó.<br/>
+</h1>`);
+
+granGozoHayEnMiAlmaHoy.push(`<h1>
+Coro<br/>
+Brilla el sol de Cristo en mi alma;<br/>
+cada día voy feliz así.<br/>
+Su faz sonriente al contemplar,<br/>
+¡cuánto gozo siento en mí!<br/>
+</h1>`);
+
+granGozoHayEnMiAlmaHoy.push(`<h1>
+En mi alma hoy reina gratitud<br/>
+y loores a Jesús.<br/>
+En su presencia hay virtud,<br/>
+hay gozo en su luz.<br/>
+</h1>`);
+
+granGozoHayEnMiAlmaHoy.push(`<h1>
+Coro<br/>
+Brilla el sol de Cristo en mi alma;<br/>
+cada día voy feliz así.<br/>
+Su faz sonriente al contemplar,<br/>
+¡cuánto gozo siento en mí!<br/>
+</h1>`);
+
+// include new song
+songs.push(granGozoHayEnMiAlmaHoy);
+
+
+
+// load lyrics for song 3
+// ----------------------
+const enCristoHalloAmigo = [];
+
+enCristoHalloAmigo.push(`<h1>
+En Cristo hallo amigo y amante Salvador;<br/>
+contare, pues, cuanto ha hecho el por mí.<br/>
+Hallándome perdido e indigno pecador,<br/>
+rescatome y hoy me guarda para si.
+</h1>`);
+
+enCristoHalloAmigo.push(`<h1>
+Me salva del pecado, me guarda de Satán,<br/>
+promete estar conmigo siempre aquí;<br/>
+consuela mi tristeza, me quita todo afán;<br/>
+grandes cosas Cristo ha hecho ya por mí.
+</h1>`);
+
+enCristoHalloAmigo.push(`<h1>
+Jesús jamás me falta, jamás me dejara;<br/>
+es mi fuerte y poderoso protector.<br/>
+Del mundo hoy me aparto, de toda vanidad,<br/>
+para consagrar la vida a mi Señor.
+</h1>`);
+
+enCristoHalloAmigo.push(`<h1>
+Si el mundo me persigue, si sufro tentación,<br/>
+confiando en el podre vencer aquí;<br/>
+segura es la victoria, y elevo mi canción:<br/>
+grandes cosas Cristo ha hecho ya por mí.
+</h1>`);
+
+enCristoHalloAmigo.push(`<h1>
+Bien sé que Cristo en gloria<br/>
+muy pronto volverá,<br/>
+y entretanto me prepara un bello hogar.<br/>
+en la casa de mi Padre,<br/>
+ mansión de luz y paz,<br/>
+el creyente fiel con el ha de morar.
+</h1>`);
+
+enCristoHalloAmigo.push(`<h1>
+Y entrado en su gloria, pesar no sentiré,<br/>
+pues contemplaré su rostro siempre allí;<br/>
+con los santos redimidos gozoso cantare:<br/>
+grandes cosas Cristo ha hecho ya por mí.
+</h1>`);
+
+// include new song
+songs.push(enCristoHalloAmigo);
+
+
+// load lyrics
+// -----------
+const gratoEsContarLaHistoria = [];
+
+gratoEsContarLaHistoria.push(`<h1>
+Grato es contar la historia<br/>
+de celestial favor,<br/>
+de Cristo y de su gloria,<br/>
+de Cristo y de su amor.
+</h1>`);
+
+gratoEsContarLaHistoria.push(`<h1>
+Me agrada referirla,<br/>
+Pues se que es la verdad;<br/>
+y nada satisface<br/>
+cual ella mi ansiedad.
+</h1>`);
+
+gratoEsContarLaHistoria.push(`<h1>
+¡Cuan bella es esta historia!<br/>
+Mi tema allá en la gloria<br/>
+será ensalzar la historia<br/>
+de Cristo y de su amor.
+</h1>`);
+
+gratoEsContarLaHistoria.push(`<h1>
+Grato es contar la historia<br/>
+mas bella que escuche,<br/>
+mas aurea, mas hermosa<br/>
+que cuanto yo soñé.
+</h1>`);
+
+gratoEsContarLaHistoria.push(`<h1>
+Contarla siempre anhelo,<br/>
+pues hay quien nunca oyó<br/>
+que, para redimirle,<br/>
+el buen Jesús murió.
+</h1>`);
+
+gratoEsContarLaHistoria.push(`<h1>
+¡Cuan bella es esta historia!<br/>
+Mi tema allá en la gloria<br/>
+será ensalzar la historia<br/>
+de Cristo y de su amor.
+</h1>`);
+
+gratoEsContarLaHistoria.push(`<h1>
+Grato es contar la historia<br/>
+que placentera es,<br/>
+y es mas, al repetirla,<br/>
+preciosa cada vez.
+</h1>`);
+
+gratoEsContarLaHistoria.push(`<h1>
+La historia, pues, que canto,<br/>
+oíd con atención,<br/>
+porque es mensaje santo<br/>
+de eterna salvación.
+</h1>`);
+
+gratoEsContarLaHistoria.push(`<h1>
+¡Cuan bella es esta historia!<br/>
+Mi tema allá en la gloria<br/>
+será ensalzar la historia<br/>
+de Cristo y de su amor.
+</h1>`);
+
+gratoEsContarLaHistoria.push(`<h1>
+Grato es contar la historia<br/>
+de todas la mejor,<br/>
+que cuanto mas se escucha,<br/>
+infunde mas amor.
+</h1>`);
+
+gratoEsContarLaHistoria.push(`<h1>
+Y cuando allá en la gloria<br/>
+entone mi cantar,<br/>
+será la misma historia<br/>
+que tanto supe amar.
+</h1>`);
+
+gratoEsContarLaHistoria.push(`<h1>
+¡Cuan bella es esta historia!<br/>
+Mi tema allá en la gloria<br/>
+será ensalzar la historia<br/>
+de Cristo y de su amor.
+</h1>`);
+
+// include new song
+songs.push(gratoEsContarLaHistoria);
+
+
+
+// load lyrics
+// -----------
+const nuestroCantoExpresaUnSentir = [];
+
+nuestroCantoExpresaUnSentir.push(`<h1>
+Nuestro canto expresa un sentir<br/>
+y es preciso que se pueda oír.<br/>
+En las calles, en las plazas,<br/>
+en los campos, las montañas,<br/>
+a lo largo y a lo ancho del país.
+</h1>`);
+
+nuestroCantoExpresaUnSentir.push(`<h1>
+Jesucristo es la verdad,<br/>
+nuestra fe y nuestro compromiso.<br/>
+Él es la vida en plenitud,<br/>
+Él es la fuerza y es la luz,<br/>
+es la alegría que me alienta a continuar.
+</h1>`);
+
+nuestroCantoExpresaUnSentir.push(`<h1>
+Es el poder para cambiar<br/>
+toda una vida y darle más.<br/>
+Jesucristo es la razón para vivir.
+</h1>`);
+
+nuestroCantoExpresaUnSentir.push(`<h1>
+Este es el momento para actuar,<br/>
+en cada rincón cada lugar.<br/>
+Nuestra patria necesita<br/>
+actitudes decididas<br/>
+de cristianos que se jueguen de verdad
+</h1>`);
+
+nuestroCantoExpresaUnSentir.push(`<h1>
+Jesucristo es la verdad,<br/>
+nuestra fe y nuestro compromiso.<br/>
+Él es la vida en plenitud,<br/>
+Él es la fuerza y es la luz,<br/>
+es la alegría que me alienta a continuar.
+</h1>`);
+
+nuestroCantoExpresaUnSentir.push(`<h1>
+Es el poder para cambiar<br/>
+toda una vida y darle más.<br/>
+Jesucristo es la razón para vivir.
+</h1>`);
+
+
+// include new song
+songs.push(nuestroCantoExpresaUnSentir);
+
+// load lyrics
+// -----------
+const aCristoCoronad = [];
+
+aCristoCoronad.push(`<h1>
+A Cristo coronad,<br/>
+divino Salvador,<br/>
+sentado en alta majestad<br/>
+es digno de loor.
+</h1>`);
+
+aCristoCoronad.push(`<h1>
+Al Rey de gloria y paz<br/>
+loores tributad,<br/>
+y bendecidle al Inmortal<br/>
+por toda eternidad
+</h1>`);
+
+aCristoCoronad.push(`<h1>
+A Cristo coronad,<br/>
+Señor de nuestro amor;<br/>
+al triunfante celebrad,<br/>
+glorioso vencedor.
+</h1>`);
+
+aCristoCoronad.push(`<h1>
+Potente Rey de paz<br/>
+el triunfo consumó,<br/>
+y por su muerte de dolor<br/>
+su grande amor mostró
+</h1>`);
+
+aCristoCoronad.push(`<h1>
+A Cristo coronad,<br/>
+Señor de vida y luz,<br/>
+con alabanzas proclamad<br/>
+los triunfos de la cruz.
+</h1>`);
+
+aCristoCoronad.push(`<h1>
+A Él, pues, adorad,<br/>
+Señor de salvación;<br/>
+loor eterno tributad<br/>
+de todo corazón
+</h1>`);
+
+// include new song
+songs.push(aCristoCoronad);
+
+// load lyrics
+// -----------
+const majestad = [];
+
+majestad.push(`<h1>
+Majestad, gloria a su majestad,<br/>
+Jesucristo merece todo honor.<br/>
+Majestad, reino, autoridad,<br/>
+suyo el honor suyo el poder gloria a Él.
+</h1>`);
+
+majestad.push(`<h1>
+Exaltad, engrandeced, su santo nombre.<br/>
+Alabad, magnificad a Cristo el rey.<br/>
+Majestad, gloria su majestad.<br/>
+Cristo murió, resucitó, hoy es Señor<br/>
+Cristo murió, resucitó, gloria a Él
+</h1>`);
+
+// include new song
+songs.push(majestad);
+
+// load lyrics
+// -----------
+const maravillosoDios = [];
+
+maravillosoDios.push(`<h1>
+Maravilloso Dios<br/>
+Tú permaneces siempre fiel<br/>
+Tu gloria y Tu poder<br/>
+llenan mi vida de Tu ser
+</h1>`);
+
+maravillosoDios.push(`<h1>
+Sostenme con Tu amor,<br/>
+con la victoria, Tu perdón<br/>
+Desde mi corazón te canto hoy
+</h1>`);
+
+// include new song
+songs.push(maravillosoDios);
+
+// load lyrics
+// -----------
+const miCristoMiRey = [];
+
+miCristoMiRey.push(`<h1>
+Mi Cristo, mi Rey<br/>
+Nadie es como Tú<br/>
+Toda mi vida<br/>
+Quiero exaltar<br/>
+Las maravillas de Tu amor
+</h1>`);
+
+miCristoMiRey.push(`<h1>
+Consuelo, refugio<br/>
+Torre de fuerza y poder<br/>
+Todo mi ser, lo que yo soy<br/>
+Nunca cese de adorar
+</h1>`);
+
+miCristoMiRey.push(`<h1>
+Cante al Señor toda la creación<br/>
+Honra y poder, majestad sea al Rey<br/>
+Montes caerán y el mar rugirá<br/>
+Al sonar de Tu nombre
+</h1>`);
+
+miCristoMiRey.push(`<h1>
+Canto con gozo al mirar Tu poder<br/>
+Por siempre yo te amaré y diré<br/>
+Incomparables promesas me das, Señor
+</h1>`);
+
+// include new song
+songs.push(miCristoMiRey);
+
+// load lyrics
+// -----------
+const padreDelCieloTeAdoramos = [];
+
+padreDelCieloTeAdoramos.push(`<h1>
+Padre del cielo te adoramos<br/>
+Y alzamos hoy tu Nombre en alto<br/>
+Que tu reino se establezca en la alabanza<br/>
+De tu pueblo que hoy declara tu poder
+</h1>`);
+
+padreDelCieloTeAdoramos.push(`<h1>
+Bendito sea el Señor Dios Poderoso<br/>
+Quién fue, Quien es y Quien vendrá<br/>
+Bendito sea el Señor Dios poderoso<br/>
+Por siempre reinará
+</h1>`);
+
+// include new song
+songs.push(padreDelCieloTeAdoramos);
+
+
+
+
+// load first song
+document.getElementById("demo").innerHTML = todasLasPromesas[song];
+
+
+
+
+// changing songs
+
+    document.addEventListener('keydown', function (event) {
+        if (event.keyCode === 39) { // arrowRight
+           
+            progress = 0;
+            console.log(progress);
+            
+            songs.length - 1 > song && ++song;
+
+            
+            document.getElementById("demo").innerHTML = songs[song][progress];
+            
+
+        }    
+    });
+
+    document.addEventListener('keydown', function (event) {
+        if (event.keyCode === 37) { // arrowLeft
+           
+            progress = 0;
+
+            // progress can't be less than 0
+            // -----------------------------
+            song > 0 && --song;            
+            
+            document.getElementById("demo").innerHTML = songs[song][progress];            
+
+        }    
+    });
+
+
+
+// manage songs and progress
+// -------------------------
+document.addEventListener('keydown', function (event) {
+    if (event.keyCode === 40) { // arrowDown
+       
+        songs[song].length - 1 > progress && ++progress;       
+        document.getElementById("demo").innerHTML = songs[song][progress];
+        
+    }    
+  });
+
+  document.addEventListener('keydown', function (event) {
+    if (event.keyCode === 38) { // arrowUp
+                
+        progress > 0 && --progress;
+        document.getElementById("demo").innerHTML = songs[song][progress];
+        
+    }
+  });
+
+
+
+
+
