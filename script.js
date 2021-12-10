2f2f60bdeb5c655d90f8025f7b3eac9cd2168c5e
Alberto Alesandroni <betoalesandroni@gmail.com>
2021-12-10 14:25:11 +0100

    add songs

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ const progressDots = document.querySelector('#progressDots');
 const todasLasPromesas = [];
 
 todasLasPromesas.push(`<h1>
+A<br/>
 Todas las promesas del Señor Jesús<br/>
 son apoyo poderoso de mi fe<br/>
 mientras viva aquí cercado de su luz<br/>
@@ -78,6 +79,7 @@ songs.push(todasLasPromesas);
 const granGozoHayEnMiAlmaHoy = [];
 
 granGozoHayEnMiAlmaHoy.push(`<h1>
+G<br/>
 Gran gozo hay en mi alma hoy:<br/>
 Jesús conmigo está;<br/>
 contento con su amor estoy,<br/>
@@ -143,6 +145,7 @@ songs.push(granGozoHayEnMiAlmaHoy);
 const enCristoHalloAmigo = [];
 
 enCristoHalloAmigo.push(`<h1>
+E<br/>
 En Cristo hallo amigo y amante Salvador;<br/>
 contare, pues, cuanto ha hecho el por mí.<br/>
 Hallándome perdido e indigno pecador,<br/>
@@ -195,6 +198,7 @@ songs.push(enCristoHalloAmigo);
 const gratoEsContarLaHistoria = [];
 
 gratoEsContarLaHistoria.push(`<h1>
+G<br/>
 Grato es contar la historia<br/>
 de celestial favor,<br/>
 de Cristo y de su gloria,<br/>
@@ -288,6 +292,7 @@ songs.push(gratoEsContarLaHistoria);
 const nuestroCantoExpresaUnSentir = [];
 
 nuestroCantoExpresaUnSentir.push(`<h1>
+D<br/>
 Nuestro canto expresa un sentir<br/>
 y es preciso que se pueda oír.<br/>
 En las calles, en las plazas,<br/>
@@ -340,6 +345,7 @@ songs.push(nuestroCantoExpresaUnSentir);
 const aCristoCoronad = [];
 
 aCristoCoronad.push(`<h1>
+C<br/>
 A Cristo coronad,<br/>
 divino Salvador,<br/>
 sentado en alta majestad<br/>
@@ -389,6 +395,7 @@ songs.push(aCristoCoronad);
 const majestad = [];
 
 majestad.push(`<h1>
+G<br/>
 Majestad, gloria a su majestad,<br/>
 Jesucristo merece todo honor.<br/>
 Majestad, reino, autoridad,<br/>
@@ -411,6 +418,7 @@ songs.push(majestad);
 const maravillosoDios = [];
 
 maravillosoDios.push(`<h1>
+G<br/>
 Maravilloso Dios<br/>
 Tú permaneces siempre fiel<br/>
 Tu gloria y Tu poder<br/>
@@ -431,6 +439,7 @@ songs.push(maravillosoDios);
 const miCristoMiRey = [];
 
 miCristoMiRey.push(`<h1>
+A<br/>
 Mi Cristo, mi Rey<br/>
 Nadie es como Tú<br/>
 Toda mi vida<br/>
@@ -466,6 +475,7 @@ songs.push(miCristoMiRey);
 const padreDelCieloTeAdoramos = [];
 
 padreDelCieloTeAdoramos.push(`<h1>
+C<br/>
 Padre del cielo te adoramos<br/>
 Y alzamos hoy tu Nombre en alto<br/>
 Que tu reino se establezca en la alabanza<br/>
@@ -481,6 +491,671 @@ Por siempre reinará
 
 // include new song
 songs.push(padreDelCieloTeAdoramos);
+
+// load lyrics
+// -----------
+const alEstarAqui = [];
+
+alEstarAqui.push(`<h1>
+A<br/>
+Al estar en la presencia de tu divinidad<br/>
+Y al contemplar la hermosura de tu santidad<br/>
+Mi espíritu se alegra en tu majestad<br/>
+Te adoro a ti, te adoro a ti
+</h1>`);
+
+alEstarAqui.push(`<h1>
+Cuando veo la grandeza de tu dulce amor<br/>
+Y compruebo la pureza de tu corazón<br/>
+Mi espíritu se alegra en tu majestad<br/>
+Te adoro a ti, te adoro a ti
+</h1>`);
+
+alEstarAqui.push(`<h1>
+Y al estar aquí, delante de ti te adorare<br/>
+Postrado ante ti,<br/>
+mi corazón te adora oh Dios<br/>
+Y siempre quiero estar<br/>
+Para adorar y contemplar tu santidad<br/>
+Te adoro a ti Señor, te adoro a ti
+</h1>`);
+
+// include new song
+songs.push(alEstarAqui);
+
+
+
+
+
+// load lyrics
+// -----------
+const salmo23 = [];
+
+salmo23.push(`<h1>
+C<br/>
+El Señor es mi pastor<br/>
+Nada me faltará<br/>
+El Señor es mi pastor<br/>
+En pastos delicados<br/>
+Él me hará descansar
+</h1>`);
+
+salmo23.push(`<h1>
+Junto a aguas de reposo<br/>
+Me pastoreará<br/>
+Confortará mi alma<br/>
+Me guiará por sendas de justicia<br/>
+Por amor de su nombre
+</h1>`);
+
+salmo23.push(`<h1>
+El Señor es mi pastor<br/>
+Nada me faltará
+</h1>`);
+
+// include new song
+songs.push(salmo23);
+
+
+
+// load lyrics
+// -----------
+const cuandoAllaSePaseLista = [];
+
+cuandoAllaSePaseLista.push(`<h1>
+G<br/>
+Cuando la trompeta suene en aquel día final<br/>
+Y que el alba eterna rompa en claridad<br/>
+Cuando las naciones salvas<br/>
+A su patria lleguén ya<br/>
+Y que sea pasada lista, allí estaré
+</h1>`);
+
+cuandoAllaSePaseLista.push(`<h1>
+Cuando allá se pase lista<br/>
+Cuando allá se pase lista<br/>
+Cuando allá se pase lista<br/>
+A mi nombre yo feliz responderé
+</h1>`);
+
+cuandoAllaSePaseLista.push(`<h1>
+En aquel día sin nieblas<br/>
+En que muerte ya no habrá<br/>
+Y su gloria el Salvador impartirá<br/>
+Cuando los llamados entren<br/>
+A su celestial hogar<br/>
+Y que sea pasada lista, allí he de estar
+</h1>`);
+
+cuandoAllaSePaseLista.push(`<h1>
+Cuando allá se pase lista<br/>
+Cuando allá se pase lista<br/>
+Cuando allá se pase lista<br/>
+A mi nombre yo feliz responderé
+</h1>`);
+
+cuandoAllaSePaseLista.push(`<h1>
+Trabajemos por el maestro<br/>
+Desde al alba al dislumbrar<br/>
+Siempre hablemos de su amor y fiel bondad<br/>
+Cuando todo aqui perezca<br/>Y
+Nuestra obra cesé ya<br/>
+Y que sea pasada lista, allí estaré
+</h1>`);
+
+cuandoAllaSePaseLista.push(`<h1>
+Cuando allá se pase lista<br/>
+Cuando allá se pase lista<br/>
+Cuando allá se pase lista<br/>
+A mi nombre yo feliz responderé
+</h1>`);
+
+// include new song
+songs.push(cuandoAllaSePaseLista);
+
+
+
+// load lyrics
+// -----------
+const enJesucristoElReayDePaz = [];
+
+enJesucristoElReayDePaz.push(`<h1>
+D<br/>
+En Jesucristo el rey de paz<br/>
+En horas negras de tempestad<br/>
+Hallan las almas dulce solaz<br/>
+Grato consuelo felicidad
+</h1>`);
+
+enJesucristoElReayDePaz.push(`<h1>
+Gloria cantemos al redentor<br/>
+Que por nosotros vino a morir<br/>
+Y que la gracia del salvador<br/>
+Siempre proteja nuestro vivir
+</h1>`);
+
+enJesucristoElReayDePaz.push(`<h1>
+En nuestras luchas, en el dolor<br/>
+En tristes horas de tentación<br/>
+Cristo nos llena de su vigor<br/>
+Y da aliento al corazón
+</h1>`);
+
+enJesucristoElReayDePaz.push(`<h1>
+Gloria cantemos al redentor<br/>
+Que por nosotros vino a morir<br/>
+Y que la gracia del salvador<br/>
+Siempre proteja nuestro vivir
+</h1>`);
+
+enJesucristoElReayDePaz.push(`<h1>
+Cuando luchamos llenos de fé<br/>
+Y no queremos desfallecer<br/>
+Cristo nos dice : "siempre os daré<br/>
+Gracias divina, santo poder"
+</h1>`);
+
+enJesucristoElReayDePaz.push(`<h1>
+Gloria cantemos al redentor<br/>
+Que por nosotros vino a morir<br/>
+Y que la gracia del salvador<br/>
+Siempre proteja nuestro vivir
+</h1>`);
+
+// include new song
+songs.push(enJesucristoElReayDePaz);
+
+
+
+// load lyrics
+// -----------
+const noHayDiosTanGrandeComoTu = [];
+
+noHayDiosTanGrandeComoTu.push(`<h1>
+E<br/>
+No hay Dios tan grande como tú<br/>
+No lo hay, no lo hay //x2
+</h1>`);
+
+noHayDiosTanGrandeComoTu.push(`<h1>
+No hay Dios que pueda hacer las obras<br/>
+Como las que haces tú //x2
+</h1>`);
+
+noHayDiosTanGrandeComoTu.push(`<h1>
+No es con espada, ni con ejercito,<br/>
+Más con tu santo espíritu //x2
+</h1>`);
+
+noHayDiosTanGrandeComoTu.push(`<h1>
+Y esas almas se salvarán<br/>
+Y esas almas se salvarán<br/>
+Y esas almas se salvarán<br/>
+Por tu santo espíritu
+</h1>`);
+
+// include new song
+songs.push(noHayDiosTanGrandeComoTu);
+
+
+
+// load lyrics
+// -----------
+const vineAAdorarADios = [];
+
+vineAAdorarADios.push(`<h1>
+C<br/>
+Vine a adorar a Dios<br/>
+Vine a adorar a Dios<br/>
+Vine a adorar su nombre<br/>
+Vine a adorar a Dios
+</h1>`);
+
+vineAAdorarADios.push(`<h1>
+El vino a mi vida<br/>
+En un día muy especial<br/>
+Cambió mi corazón<br/>
+En un nuevo corazón
+</h1>`);
+
+vineAAdorarADios.push(`<h1>
+Y esta es la razón<br/>
+Por la que digo que<br/>
+Vine a adorar a Dios<br/>
+Vine a adorar a Dios
+</h1>`);
+
+// include new song
+songs.push(vineAAdorarADios);
+
+
+
+// load lyrics
+// -----------
+const yoVivoSeñorPorqueTuVives = [];
+
+yoVivoSeñorPorqueTuVives.push(`<h1>
+D<br/>
+Yo vivo Señor porque tú vives<br/>
+Porque tú vives, Señor es que yo vivo<br/><br/>
+// Me das consuelo, me das abrigo<br/>
+Y en la aflicción, mi Señor estás conmigo //
+</h1>`);
+
+yoVivoSeñorPorqueTuVives.push(`<h1>
+Soy salvo Señor pues me salvaste<br/>
+Pues me salvaste, Señor eternamente<br/><br/>
+// Yo voy al cielo, voy a la gloria<br/>
+porque Señor tu me diste la victoria //
+</h1>`);
+
+// include new song
+songs.push(yoVivoSeñorPorqueTuVives);
+
+
+
+// load lyrics
+// -----------
+const miradCualAmor = [];
+
+miradCualAmor.push(`<h1>
+E<br/>
+Mirad cual amor nos ha dado el Padre<br/>
+Al hacernos hijos de Dios<br/>
+Mirad cual amor nos ha dado el Padre<br/>
+Al hacernos hijos de Dios<br/>
+</h1>`);
+
+miradCualAmor.push(`<h1>
+Para ser llamados hijos de Dios<br/>
+Para ser llamados hijos de Dios
+</h1>`);
+
+// include new song
+songs.push(miradCualAmor);
+
+
+
+// load lyrics
+// -----------
+const renuevameSeñorJesus = [];
+
+renuevameSeñorJesus.push(`<h1>
+D<br/>
+Renuévame señor Jesús<br/>
+Ya no quiero ser igual<br/>
+Renuévame señor Jesús<br/>
+Pon en mí tu corazón
+</h1>`);
+
+renuevameSeñorJesus.push(`<h1>
+Porque todo lo que hay dentro de mí<br/>
+Necesita ser cambiado Señor<br/>
+Porque todo lo que hay<br/>
+Dentro de mí corazón<br/>
+Necesita más de tí
+</h1>`);
+
+// include new song
+songs.push(renuevameSeñorJesus);
+
+
+
+// load lyrics
+// -----------
+const hayPoder = [];
+
+hayPoder.push(`<h1>
+G<br/>
+Hay poder, poder sin igual poder<br/>
+En Jesús quién murió<br/>
+Hay poder, poder sin igual poder<br/>
+En la sangre que Él perdió
+</h1>`);
+
+hayPoder.push(`<h1>
+¿Quieres ser salvo de toda maldad?<br/>
+Tan solo hay poder en mi Jesús<br/>
+¿Quieres vivir y gozar santidad?<br/>
+Tan solo hay poder en Jesús
+</h1>`);
+
+hayPoder.push(`<h1>
+Hay poder, poder sin igual poder<br/>
+En Jesús quién murió<br/>
+Hay poder, poder sin igual poder<br/>
+En la sangre que Él perdió
+</h1>`);
+
+hayPoder.push(`<h1>
+¿Quieres ser libre de toda maldad?<br/>
+Tan solo hay poder en mi Jesús<br/>
+¿Quieres vencer toda cruel tentación?<br/>
+Tan solo hay poder en Jesús
+</h1>`);
+
+hayPoder.push(`<h1>
+Hay poder, poder sin igual poder<br/>
+En Jesús quién murió<br/>
+Hay poder, poder sin igual poder<br/>
+En la sangre que Él perdió
+</h1>`);
+
+// include new song
+songs.push(hayPoder);
+
+
+// load lyrics
+// -----------
+const suGraciaEsMayor = [];
+
+suGraciaEsMayor.push(`<h1>
+F<br/>
+¿Que amor mis pecados decide olvidar?<br/>
+Lanzados al mar no los quiere contar<br/>
+El siendo Omnisciente olvida mi error<br/>
+Mis faltas son muchas<br/>
+Su gracia es Mayor
+</h1>`);
+
+suGraciaEsMayor.push(`<h1>
+Gloria Dios, su gracia es mayor<br/>
+Sus misericordias hoy nuevas son<br/>
+Mis faltas son muchas<br/>
+Su gracia es mayor
+</h1>`);
+
+suGraciaEsMayor.push(`<h1>
+Paciente me aguarda en mi desviar<br/>
+Un padre que tierno me llama al hogar<br/>
+Recibe al débil y vil pecador<br/>
+Mis faltas son muchas<br/>
+Su Gracia es Mayor
+</h1>`);
+
+suGraciaEsMayor.push(`<h1>
+Gloria Dios, su gracia es mayor<br/>
+Sus misericordias hoy nuevas son<br/>
+Mis faltas son muchas<br/>
+Su gracia es mayor
+</h1>`);
+
+suGraciaEsMayor.push(`<h1>
+Que grandes riquezas Él nos otorgó<br/>
+Su sangre fue el pago, su vida entregó<br/>
+Pago esa deuda y la canceló<br/>
+Mis faltas son muchas<br/>
+Su gracia es mayor
+</h1>`);
+
+suGraciaEsMayor.push(`<h1>
+Gloria Dios, su gracia es mayor<br/>
+Sus misericordias hoy nuevas son<br/>
+Mis faltas son muchas<br/>
+Su gracia es mayor
+</h1>`);
+
+
+// include new song
+songs.push(suGraciaEsMayor);
+
+
+// load lyrics
+// -----------
+const tuDejasteTuTrono = [];
+
+tuDejasteTuTrono.push(`<h1>
+F<br/>
+Tú dejaste tu trono y corona por mí<br/>
+al venir a Belén a nacer.<br/>
+Mas a Ti no fue dado el entrar al mesón,<br/>
+y en pesebre te hicieron yacer.
+</h1>`);
+
+tuDejasteTuTrono.push(`<h1>
+Ven a mi corazón, oh Cristo,<br/>
+pues en él hay lugar para Ti.<br/>
+Ven a mi corazón, oh Cristo, ven,<br/>
+pues en él hay lugar para Ti.
+</h1>`);
+
+tuDejasteTuTrono.push(`<h1>
+Tú viniste, Señor, con tu gran bendición,<br/>
+para dar libertad y salud.<br/>
+Mas con odio y desprecio te hicieron morir,<br/>
+aunque vieron tu amor y virtud.
+</h1>`);
+
+tuDejasteTuTrono.push(`<h1>
+Ven a mi corazón, oh Cristo,<br/>
+pues en él hay lugar para Ti.<br/>
+Ven a mi corazón, oh Cristo, ven,<br/>
+pues en él hay lugar para Ti.
+</h1>`);
+
+tuDejasteTuTrono.push(`<h1>
+Alabanzas sublimes los cielos darán<br/>
+cuando vengas glorioso de allí,<br/>
+y tu voz nos dirá de las nubes: Venid<br/>
+al hogar celestial junto a mí
+</h1>`);
+
+tuDejasteTuTrono.push(`<h1>
+Ven a mi corazón, oh Cristo,<br/>
+pues en él hay lugar para Ti.<br/>
+Ven a mi corazón, oh Cristo, ven,<br/>
+pues en él hay lugar para Ti.
+</h1>`);
+
+// include new song
+songs.push(tuDejasteTuTrono);
+
+
+// load lyrics
+// -----------
+const angelesCantandoEstan = [];
+
+angelesCantandoEstan.push(`<h1>
+F<br/>
+Ángeles cantando están<br/>
+Tan dulcísima canción;<br/>
+Las montañas su eco dan<br/>
+Como fiel contestación.
+</h1>`);
+
+angelesCantandoEstan.push(`<h1>
+Gloria, en lo alto gloria,<br/>
+Gloria, en lo alto gloria a Dios.<br/>
+</h1>`);
+
+angelesCantandoEstan.push(`<h1>
+Los pastores sin cesar<br/>
+Sus loores dan a Dios;<br/>
+Cuán glorioso es el cantar<br/>
+De su melodiosa voz.
+</h1>`);
+
+angelesCantandoEstan.push(`<h1>
+Gloria, en lo alto gloria,<br/>
+Gloria, en lo alto gloria a Dios.
+</h1>`);
+
+angelesCantandoEstan.push(`<h1>
+Oh, venid pronto a Belén<br/>
+Para contemplar con fe<br/>
+A Jesús, autor del bien,<br/>
+Al recién nacido Rey.
+</h1>`);
+
+angelesCantandoEstan.push(`<h1>
+Gloria, en lo alto gloria,<br/>
+Gloria, en lo alto gloria a Dios.
+</h1>`);
+
+// include new song
+songs.push(angelesCantandoEstan);
+
+
+// // load lyrics
+// // -----------
+const oidUnSonEnAltaEsfera = [];
+
+oidUnSonEnAltaEsfera.push(`<h1>
+G<br/>
+Oíd un son en la alta esfera:<br/>
+En los cielos gloria a Dios;<br/>
+Al mortal paz en la tierra,<br/>
+Canta la celeste voz.
+</h1>`);
+
+oidUnSonEnAltaEsfera.push(`<h1>
+Con los cielos alabemos<br/>
+Al eterno Rey; cantemos<br/>
+A Jesús, que es nuestro bien,<br/>
+Con el coro de Belén.<br/>
+Canta la celeste voz:<br/>
+¡En los cielos gloria a Dios!
+</h1>`);
+
+oidUnSonEnAltaEsfera.push(`<h1>
+El Señor de los señores,<br/>
+El Ungido celestial,<br/>
+A salvar los pecadores<br/>
+Vino al mundo terrenal.
+</h1>`);
+
+oidUnSonEnAltaEsfera.push(`<h1>
+Loor al Verbo encarnado,<br/>
+En humanidad velado;<br/>
+Gloria al Santo de Israel,<br/>
+Cuyo nombre es Emanuel.<br/>
+Canta la celeste voz:<br/>
+¡En los cielos gloria a Dios!
+</h1>`);
+
+oidUnSonEnAltaEsfera.push(`<h1>
+Príncipe de paz eterna,<br/>
+¡Gloria sea a Ti, Jesús!<br/>
+De tu heredad paterna<br/>
+Nos trajiste vida y luz.
+</h1>`);
+
+oidUnSonEnAltaEsfera.push(`<h1>
+Has tu majestad dejado,<br/>
+Y buscarnos te has dignado;<br/>
+Para darnos el vivir,<br/>
+A la muerte osaste ir.<br/>
+Canta la celeste voz:<br/>
+¡En los cielos gloria a Dios!
+</h1>`);
+
+// include new song
+songs.push(oidUnSonEnAltaEsfera);
+
+
+// load lyrics
+// -----------
+const venidFielesTodos = [];
+
+venidFielesTodos.push(`<h1>
+F<br/>
+Venid fieles todos a Belén marchemos<br/>
+De gozo triunfantes y llenos de amor<br/>
+Y al Rey de los cielos humilde veremos
+</h1>`);
+
+venidFielesTodos.push(`<h1>
+Venid y adoremos<br/>
+Venid y adoremos<br/>
+Venid y adoremos a Cristo el Señor
+</h1>`);
+
+venidFielesTodos.push(`<h1>
+En pobre pesebre yace reclinado<br/>
+Al hombre ofreciendo eterna salvación<br/>
+El Santo Mesias el verbo encarnado
+</h1>`);
+
+venidFielesTodos.push(`<h1>
+Venid y adoremos<br/>
+Venid y adoremos<br/>
+Venid y adoremos a Cristo el Señor
+</h1>`);
+
+// include new song
+songs.push(venidFielesTodos);
+
+
+// // load lyrics
+// // -----------
+const nocheDePaz = [];
+
+nocheDePaz.push(`<h1>
+¡Noche de paz, noche de amor!<br/>
+Todo duerme en derredor.<br/>
+Entre los astros que esparcen su luz,<br/>
+Bella anunciando al niño Jesús,<br/>
+Brilla la estrella de paz, brilla la estrella de paz.
+</h1>`);
+
+nocheDePaz.push(`<h1>
+¡Noche de paz, noche de amor!<br/>
+Oye humilde el fiel pastor<br/>
+Coros celestes que anuncian salud,<br/>
+Gracias y glorias en gran plenitud,<br/>
+Por nuestro buen Redentor, por nuestro buen Redentor.
+</h1>`);
+
+nocheDePaz.push(`<h1>
+¡Noche de paz, noche de amor!<br/>
+Ved qué bello resplandor<br/>
+Luce en el rostro del niño Jesús,<br/>
+En el pesebre, del mundo la luz,<br/>
+Astro de eterno fulgor, astro de eterno fulgor.
+</h1>`);
+
+// include new song
+songs.push(nocheDePaz);
+
+
+// load lyrics
+// -----------
+const hoSantisimoFelicisimo = [];
+
+hoSantisimoFelicisimo.push(`<h1>
+F<br/>
+Oh! santísimo, felicísimo,<br/>
+Grato tiempo de Navidad!<br/>
+Al mundo perdido<br/>
+Cristo le ha nacido:<br/>
+¡Alegría, alegría, cristiandad!
+</h1>`);
+
+hoSantisimoFelicisimo.push(`<h1>
+Oh! santísimo, felicísimo,<br/>
+Grato tiempo de Navidad!<br/>
+Coros celestiales<br/>
+Oyen los mortales:<br/>
+¡Alegría, alegría, cristiandad!
+</h1>`);
+
+hoSantisimoFelicisimo.push(`<h1>
+Oh! santísimo, felicísimo,<br/>
+Grato tiempo de Navidad!<br/>
+Príncipe del cielo,<br/>
+Danos tu consuelo:<br/>
+¡Alegría, alegría, cristiandad! 
+</h1>`);
+
+// include new song
+songs.push(hoSantisimoFelicisimo);
+
+
+
+
+
+
+
 
 
 
